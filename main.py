aab1d77c70d41132f37f75675653c95413e4f9e7
Jose <jose@teste.com>
2024-06-20 15:39:58 -0300

    mudanca do main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 from operacoes import soma
 from geradordetexto import apresentacao
+from listas import gerandoListas
 
 print(soma(1,2))
 print(apresentacao('Felipe'))
-print(apresentacao('Felipe'))
+print(gerandoListas(['1']))
