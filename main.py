b491100d442fae2772bb6255633affbb471f0758
Carlos <felipe_nascimento_2@hotmail.com>
2024-06-19 21:33:29 -0300

    Iniciando com modulos

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-from dataclasses import dataclass
-
-@dataclass
-class Cliente:
-    nome: str
-    email: str
-    idade: int
-
-    def exibir(self):
-        print(self.nome, self.email, self.idade)
-
-felipe = Cliente(nome='Felipe', email='teste@teste', idade=32)
-
-felipe.exibir()
+# chamando funcoes
